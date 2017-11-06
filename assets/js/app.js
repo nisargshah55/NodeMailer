19d640e534a115e838146ee59d6a7e6f1718d15c
Nisarg Shah <test@youremail.com>
2017-11-05 20:21:27 -0500

    Code for Node Mailer API

---
+++ assets/js/app.js
@@ -0,0 +1,163 @@
+var app = angular.module("myapp",['ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+
+    .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+    })
+
+    .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'RegisterCtrl'
+    })
+
+    .when('/', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+    })
+
+    .when('/home', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
+    })
+
+    .when('/profile', {
+        templateUrl: 'views/profile.html',
+        controller: 'ProfileCtrl'
+    })
+
+    .when('/message', {
+        templateUrl: 'views/message.html',
+        controller: 'MessageCtrl'
+    })
+
+    .otherwise({ redirectTo: '/login' });
+}])
+
+
+app.service('updateService', ['$http','$rootScope', function ($http,$rootScope) {
+    this.populate_profile = function() {
+        console.log("It works!!");
+        $http.get('/profile').success(function(response) {
+            $rootScope.user = response;
+        })
+    }
+
+    this.update_profile = function() {
+        console.log("Update works!!");
+        $http.put('/profile').success(function(response) {
+            $rootScope.user = response;
+        })
+    }
+}])
+
+
+app.controller('LoginCtrl', ['$scope','$location','$http', function ($scope,$location,$http) {
+
+    $scope.register = function() {
+        $location.path("/register");
+    }
+
+    $scope.login = function() {
+        console.log($scope.user);
+
+        $http.post("/login",$scope.user).success(function(response) {
+            if(response === "success"){
+            $location.path("/home");
+            }
+            else{
+                $scope.message = "Username or password incorrect";
+            }
+            $scope.user = "";
+        })
+    }
+    
+}])
+
+app.controller('RegisterCtrl', ['$scope','$location','$http', function ($scope,$location,$http) {
+   
+   $scope.login = function() {
+        $location.path("/login");
+    } 
+
+    $scope.register = function() {
+        console.log($scope.user);
+
+        $http.post("/register",$scope.user).success(function(response) {
+            if(response === "success"){
+                $location.path("/login");
+            }else{
+                $scope.message = "Please try again."
+            }
+            $scope.user = "";
+        })
+    }
+}])
+
+app.controller('HomeCtrl', ['$scope','updateService', function ($scope,updateService) {
+     $scope.populate_profile = function() {
+        updateService.populate_profile();
+    }
+}])
+
+app.controller('ProfileCtrl', ['$scope','$http','$location', function ($scope,$http,$location) {
+    
+        //updateService.populate_profile();
+        $http.get('/profile').success(function(response) {
+            console.log(response);
+
+            $scope.user = response;
+        })
+    
+
+    
+        $scope.update = function() {
+           // updateService.update_profile();
+          $http.put('/update/'+ $scope.user._id, $scope.user).success(function(response) {
+            console.log($scope.user._id);
+            if(response === "successful"){
+            
+            $scope.message = "Profile updated successfully";
+            $location.path("/profile");
+           // console.log("Profile updated");
+        }else{
+            $scope.message = "Error";
+        }
+           // $window.location.reload();
+            //$scope.stu = "";
+        })
+    }
+
+}])
+
+app.controller('MessageCtrl', ['$scope','$http', function ($scope,$http) {
+    $scope.send_email = function () {
+        $scope.message = "Sending E-mail...Please wait";
+        var to = $scope.to;
+        console.log(to);
+        var subject = $scope.subject;
+        var content = $scope.content;   
+$http({
+    url: "/send",
+    method: "GET",
+    params: {to:to,subject:subject,content:content}
+ }).success(function(serverResponse) {
+console.log(serverResponse);
+if(serverResponse == "sent") {
+    $scope.message = "";
+    $scope.message = "Email is been sent at "+to+" . Please check inbox!";
+}
+}).error(function(serverResponse) {
+console.log(serverResponse);
+});
+    $scope.to = "";
+    $scope.subject = "";
+    $scope.content = "";
+};
+
+    $scope.close = function() {
+        $scope.message = "";
+    }
+}])
---
+++ server.js
@@ -0,0 +1,178 @@
+var mongodb = require('mongodb'),
+    mongoClient = mongodb.MongoClient,
+    express = require('express'),
+    app = express(),
+    bodyparser = require('body-parser');
+var session = require('express-session'),
+ MongoDBStore = require('connect-mongodb-session')(session);
+var nodemailer = require("nodemailer");
+
+var conn = "mongodb://localhost:27017/mean";
+var store = new MongoDBStore({
+    uri:"mongodb://localhost:27017/mean",
+    collection:'login_session'
+});
+
+var new_db = new mongodb.Db('mean', new mongodb.Server('localhost', 27017));
+new_db.open(function (err, db) {
+    if(!err){
+        console.log("Create Database!");
+    }
+    db.close();
+});
+
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.static('assets'));
+app.use(
+    session({
+        secret:'test_sess_secret_key',
+        resave:true,
+        saveUninitialized:true,
+        store:store
+    })
+);
+
+store.on('error', function (req, res) {
+    console.log("error");
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post("/register",function(req,res) {
+	mongoClient.connect(conn, function(err, db){
+        if(err){
+            console.log(err);
+            res.send("Error happened while connecting to the database");
+        }
+        else {
+		console.log(req.body.username);
+		db.collection("register").insert(req.body, function(err,records) {
+		if(records){
+			res.send("success");
+		}else{
+			res.send("error");
+		}
+		db.close();
+	})
+	}	
+})
+})
+
+app.post("/login", function(req,res) {
+	mongoClient.connect(conn, function(err, db){
+        if(err){
+            console.log(err);
+            res.send("Error happened while connecting to the database");
+        }
+        else {
+		console.log(req.body.username);
+		db.collection("register").findOne(req.body, function(err,records) {
+			if(records){
+			req.session.loguser = records;
+			res.send("success");
+		}else{
+			res.send("error");
+		}
+		db.close();
+		})
+	}
+})
+})
+
+app.get("/profile", function(req,res) {
+	mongoClient.connect(conn, function(err,db) {
+		if(err){
+            console.log(err);
+            res.send("Error happened while connecting to the database");
+        }
+        else {
+
+        	db.collection("register").find({}).toArray(function(err,records) {
+        	if(records){
+        		console.log(req.session.loguser.location);
+        		console.log(records);
+        		for(var i in records){
+        		if(records[i].username == req.session.loguser.username)
+        		res.send(JSON.stringify(records[i]));
+        	}
+				//res.send("success");
+			}else{
+				res.send("error");
+			}
+			db.close();
+	        	});
+        }	
+	})
+})
+
+
+app.put("/update/:id", function(req,res) {
+	mongoClient.connect(conn, function(err,db) {
+		if(err){
+            console.log(err);
+            res.send("Error happened while connecting to the database");
+        }
+        else {
+
+        	var id = req.params.id;
+        	console.log("Id is "+id);
+        	console.log("User is "+req.body.username);
+        	var o_id =new mongodb.ObjectID(id);
+        	db.collection("register").update({"_id":o_id},
+        		{$set: {name: req.body.name, contact: req.body.contact,username: req.body.username,
+        		password:req.body.password, location: req.body.location, contact: req.body.contact}},
+    	 function (err, records) {
+    	 	if(records){
+    	 		req.session.loguser = records;
+      	res.send('successful');
+      	}
+    })
+
+        	}
+        })
+})
+
+
+var smtpTransport = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+        user: '',
+        pass: ''
+    },
+    tls: {rejectUnauthorized: false},
+    debug:true
+});
+
+
+/*------------------SMTP Over-----------------------------*/
+
+/*------------------Routing Started ------------------------*/
+
+
+app.get('/send',function(req,res){
+	var mailOptions={
+		from:'"Nisarg Shah" <nisargshah5408@gmail.com>',
+		to : req.query.to,
+		subject : req.query.subject,
+		text : req.query.content
+	}
+	console.log(mailOptions);
+	smtpTransport.sendMail(mailOptions, function(error, response){
+   	 if(error){
+        	console.log(error);
+		res.end("error");
+	 }else{
+        	console.log("Message sent: " + response.message);
+		res.end("sent");
+    	 }
+});
+});
+
+app.listen(3000);
+console.log("Server listening on port 3000");
